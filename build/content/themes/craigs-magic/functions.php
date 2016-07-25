dd13b5ad8783de680a981ded75fbebfcb90267c9
James Howard <james@reetgood.co.uk>
2016-07-25 11:17:33 +0100

    Enable WordPress post thumbnails.

--- build/content/themes/craigs-magic/functions.php
+++ build/content/themes/craigs-magic/functions.php
@@ -2,15 +2,18 @@
 
 use \Timber\Timber;
 
+// Enable post thumbnails (featured images).
+add_theme_support('post-thumbnails');
+
 // Remove things WordPress thinks we need that we don't.
 remove_action('wp_head', 'print_emoji_detection_script', 7);
 remove_action('wp_print_styles', 'print_emoji_styles');
 
 // Styles.
-function ccEnqueueStyles() {
+function rgEnqueueStyles() {
 	wp_enqueue_style('main', get_template_directory_uri() . '/css/style.min.css'); // Remove .min to use unminified CSS for testing.
 }
-add_action('wp_enqueue_scripts', 'ccEnqueueStyles');
+add_action('wp_enqueue_scripts', 'rgEnqueueStyles');
 
 // Timber.
 Timber::$dirname = array('templates');
