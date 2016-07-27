407e3fe772e0f09d7682a970a2e9b2502149356f
James Howard <james@reetgood.co.uk>
2016-07-27 13:18:04 +0100

    Add testimonial to news article page.

--- build/content/themes/craigs-magic/single.php
+++ build/content/themes/craigs-magic/single.php
@@ -7,4 +7,13 @@ $templates = array('single.twig');
 
 $context['post'] = Timber::get_post();
 
+// Add a random testimonial.
+$testimonials = Timber::get_posts(array(
+	'post_type'      => 'testimonial',
+	'posts_per_page' => 1,
+	'orderby'        => 'rand'
+));
+
+$context['testimonial'] = (count($testimonials) > 0) ? $testimonials[0] : NULL;
+
 Timber::render($templates, $context);
